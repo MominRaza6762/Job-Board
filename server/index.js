691479febf1875fcda902d13746a94989cacf9a2
Momin Raza <135518733+MominRaza6762@users.noreply.github.com>
2025-03-07 12:39:57 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 const app =express();
 app.use(cors({
-    origin:  ["http://localhost:5173", "http://192.168.18.18:5173"],
+    origin:  ["https://job-board-client-delta.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true
 }))
